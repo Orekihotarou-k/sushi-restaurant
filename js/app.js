b2f49c0844bb1051e901a73b490eda3e387de750
Levis Kim <122298089+Orekihotarou-k@users.noreply.github.com>
2023-12-27 18:45:44 +0100

    update styles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const navToggle = document.querySelector(".menu-toggle")
 const primaryNav = document.querySelector(".primary-nav")
 const navOpen = document.querySelector(".nav-open")
+const hamburger = document.querySelector(".hamburger")
 
 navToggle.addEventListener("click", () => {
     menuToggle()
@@ -9,9 +10,11 @@ navToggle.addEventListener("click", () => {
 function menuToggle() {
     if (primaryNav.classList.contains("primary-nav--active")) {
         primaryNav.classList.remove("primary-nav--active")
+        hamburger.classList.remove("hamburger--active")
         navOpen.style.overflow = "hidden"
     } else {
         primaryNav.classList.add("primary-nav--active")
+        hamburger.classList.add("hamburger--active")
         navOpen.style.overflow = "visible"
     }
 }
